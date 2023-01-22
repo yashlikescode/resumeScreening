f69e4ca4604d2cd092de58bc958565da47ab2769
Yashasvi <66861659+stlyash@users.noreply.github.com>
2023-01-22 10:54:16 +0530

    Update resumeScreening.py

--- resumeScreening.py
+++ resumeScreening.py
@@ -1,13 +1,37 @@
-import docx2txt
-import PyPDF2
-import os
+import docx2txt     # to convert docx files into text
+import PyPDF2       # to convert pdf files into text
+import os           # to read files from a folder/directory
+import pandas as pd
 
+def sort2list(list1, list2):
+    zipped_pairs = zip(list2, list1)
+    z = [x for _, x in sorted(zipped_pairs)]
+    return z
 
-path = r'C:\Users\kmrya\Desktop\screeningResume\inputFolder'
-os.chdir(path)
- 
-fileName = 'selected4.pdf'
-for fileName in os.listdir():
+keyfile = open('keywords.txt','r')
+keyWords = keyfile.read()
+keyWords = keyWords.lower()
+keyfile.close()
+keylist = list(keyWords.split(','))
+keylen = len(keylist)
+
+pathinp = r'.\inputFolder'
+pathparent = r'..'
+os.chdir(pathinp)
+
+fileList = os.listdir()
+fileScore = []
+fileLen = len(fileList)
+
+files = 0
+indicator = 0
+for fileName in fileList:
+    files+=1
+    if files%(fileLen//4) == 0:
+        indicator+=25
+        print("Processed {}%".format(indicator))
+
+    keyfound = 0
     text=''
     try:
         resume = docx2txt.process(fileName)
@@ -22,9 +46,22 @@ for fileName in os.listdir():
             for i in tex:
                 if type(i)==str:
                     text+=i
-    for i in text:
-        try:
-            print(i,end='')
-        except:
-            print("",end='')
+        file.close()
 
+    text = text.lower()
+    for ky in keylist:
+        if text.find(ky) != -1:
+            keyfound += 1
+    fileScore.append(keyfound*100/keylen)
+
+os.chdir(pathparent)
+
+fileList = sort2list(fileList,fileScore)
+fileScore.sort()
+
+fileList.reverse()
+fileScore.reverse()
+
+df = pd.DataFrame({"File Names" : fileList, r"% Keywords Found" : fileScore})
+df=df[df[r"% Keywords Found"]!=0]
+df.to_csv("resumeResult.csv", index=False)
